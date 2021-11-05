c058cb3c34f8e20bd72cb2329020f0e89388343e
GowthamR7899435252 <gautham.n12@gmail.com>
2021-11-05 22:38:19 +0530

    Evennumbers

---
+++ Web-Automation/src/TestScripts/Evennumbers.java
@@ -0,0 +1,17 @@
+package TestScripts;
+
+public class Evennumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("even numbers"+i);
+			}
+		}
+	}
+
+}
