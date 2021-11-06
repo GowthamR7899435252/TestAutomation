8fbc7e5a3f6b4f5f81c3857ba5d8ffe5fc08928a
GowthamR7899435252 <gautham.n12@gmail.com>
2021-11-06 16:56:01 +0530

    Evennumbers

--- Web-Automation/src/TestScripts/Evennumbers.java
+++ Web-Automation/src/TestScripts/Evennumbers.java
@@ -5,7 +5,7 @@ public class Evennumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for(int i=0;i<25;i++)
+		for(int i=0;i<35;i++)
 		{
 			if(i%2==0)
 			{
